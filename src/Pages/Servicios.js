68cbaa9ecf859bdd529114a56c062d531253cb29
Felipe Augusto <felipeaap9@gmail.com>
2025-05-17 18:58:09 +0200

    pagina Servicios

--- src/Pages/Servicios.js
+++ src/Pages/Servicios.js
@@ -1,4 +1,9 @@
 import style from "./Servicios.module.css"
+import contruccion from "../Components/images/construccion.jpg"
+import reforma from "../Components/images/reforma.jpg"
+import demolicion from "../Components/images/demolicion.jpg"
+import manteniemento from "../Components/images/manteniemento.jpg"
+
 
 
 
@@ -7,19 +12,28 @@ const Servicios = () => {
     const servicios = [
         {
             titulo: "Constuccion",
-            descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quo nisi. Eveniet nam libero ex illo, architecto optio cumque hic? Deleniti, explicabo doloribus quae laudantium quis laboriosam maxime possimus rem!"
+            subtitulo:"Diseñamos y construimos tu casa a tu medida",
+            descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quo nisi. Eveniet nam libero ex illo, architecto optio cumque hic? Deleniti, explicabo doloribus quae laudantium quis laboriosam maxime possimus rem!",
+            image: contruccion
+        
         },
         {
             titulo: "Reformas",
-            descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quo nisi. Eveniet nam libero ex illo, architecto optio cumque hic? Deleniti, explicabo doloribus quae laudantium quis laboriosam maxime possimus rem!"
+            subtitulo:"Servicios a tu medida",
+            descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quo nisi. Eveniet nam libero ex illo, architecto optio cumque hic? Deleniti, explicabo doloribus quae laudantium quis laboriosam maxime possimus rem!",
+            image: reforma
         },
         {
             titulo: "Escavacciones y Demoliciones",
-            descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quo nisi. Eveniet nam libero ex illo, architecto optio cumque hic? Deleniti, explicabo doloribus quae laudantium quis laboriosam maxime possimus rem!"
+            subtitulo:"",
+            descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quo nisi. Eveniet nam libero ex illo, architecto optio cumque hic? Deleniti, explicabo doloribus quae laudantium quis laboriosam maxime possimus rem!",
+            image: demolicion
         },
         {
-            titulo: "Manteniementos",
-            descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quo nisi. Eveniet nam libero ex illo, architecto optio cumque hic? Deleniti, explicabo doloribus quae laudantium quis laboriosam maxime possimus rem!"
+            titulo: "Mantenimiento Preventivo",
+            subtitulo:"Cuidamos para que siempre perdure",
+            descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quo nisi. Eveniet nam libero ex illo, architecto optio cumque hic? Deleniti, explicabo doloribus quae laudantium quis laboriosam maxime possimus rem!",
+            image: manteniemento
         }
         
     ]
@@ -28,7 +42,19 @@ const Servicios = () => {
     return (
         <div class="container">
             <div className={`${style.containerPrincipal}`}>
-                <p className="pagina">Hola, soy la pagina de Servicios 😀</p>
+                {servicios.map(servicio => (
+                    <div className={style.containerServicios}>
+                        <div className={style.containerText}>
+                            <h2>{servicio.titulo}</h2>
+                            <h3>{servicio.subtitulo}</h3>
+                            <p>{servicio.descripcion}</p>
+                        </div>
+                        <div className={style.containerImage}>
+                            <img src={servicio.image}/>
+                        </div>
+                    </div>
+
+                ))}
             </div>
         </div>
     )
