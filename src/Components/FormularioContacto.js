b8fea8cbd93ea8f20c74a778aa7a5c6af0186793
Felipe Augusto <felipeaap9@gmail.com>
2025-05-19 22:15:05 +0200

    vinculo EmailJS con formulario de contacto

--- src/Components/FormularioContacto.js
+++ src/Components/FormularioContacto.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import style from './FormularioContacto.module.css'
-
+import emailjs from 'emailjs-com';
+import { useRef } from "react";
 
 
 
@@ -11,6 +12,9 @@ const FormularioContacto = () => {
     const [email, setEmail] = useState("")
     const [boxText, setBoxText] = useState("")
     const [btnDesabilitado, setBtnDesabilitado] = useState(true)
+    
+    const formData = { name, phone, email, boxText }
+    const formRef = useRef()
 
 
     const cambiarNombre = (evento) => { setName(evento.target.value) }
@@ -43,28 +47,42 @@ const FormularioContacto = () => {
         setBtnDesabilitado(!validacion)
     }, [name, email, phone, boxText])
 
-
+    
 
     const btnEnviar = () => {
         console.log("formulario enviado")
         
+         emailjs.sendForm('service_6j8vi4a', 'template_ffhm9b9', formRef.current, 'yILpQCfViCC1jSqTQ')
+            .then((response) => {
+                console.log('Correo enviado con éxito!', response.status, response.text);
+                setName("");
+                setPhone("");
+                setEmail("");
+                setBoxText("")
+
+                })
+            .catch((err) => {
+                console.error('Error al enviar el correo:', err);
+            });
+
+
     }
 
 
     return  <div className={`${style.containerPrincipal}`}>
-            <form action="">
+            <form ref={formRef}>
                 <div>
-                    <input type="text" value={name} placeholder="Nombre" onChange={cambiarNombre}/>
+                    <input type="text" name="name" value={name} placeholder="Nombre" onChange={cambiarNombre}/>
 
                 </div>
                 <div>
-                    <input type="tel" value={phone} placeholder="Telefono" onChange={cambiarTelefono}/>
+                    <input type="tel" name="phone" value={phone} placeholder="Telefono" onChange={cambiarTelefono}/>
                 </div>
                 <div>
-                    <input type="text" value={email} placeholder="email" onChange={cambiarEmail}/>
+                    <input type="text" name="email" value={email} placeholder="email" onChange={cambiarEmail}/>
                 </div>
                 <div>
-                    <textarea rows="10" value={boxText} placeholder="Escriba su mensaje" onChange={cambiarBoxText}></textarea>
+                    <textarea name="message" rows="10" value={boxText} placeholder="Escriba su mensaje" onChange={cambiarBoxText}></textarea>
                 </div>
                 <button type="button" disabled={btnDesabilitado} onClick={btnEnviar}>Enviar</button>
             </form>
